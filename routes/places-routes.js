045000b268efbc26de8205491f394baee5318870
Aaron Yu <ayybuddyy@gmail.com>
2021-03-30 02:32:26 -0400

    Create Controllers for routes to separate logic, create CRUD operations for places

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,46 +1,13 @@
 const express = require('express');
 
-const HttpError = require('../models/http-error');
+const placesControllers = require('../controllers/places-controllers');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Philadelphia Art Museum',
-    description: 'Rocky made this world famous by running up the steps',
-    location: {
-      lat: 39.9655697,
-      lng: -75.1831548,
-    },
-    address: '2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130',
-    creator: 'u1',
-  },
-];
-
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    return next(new HttpError('Could not find a provided id', 404));
-  }
-
-  res.json({ place });
-});
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creatoer === userId;
-  });
-
-  if (!place) {
-    throw new HttpError('Could not find a provided id', 404);
-  }
-  res.json({ place });
-});
+router.get('/:pid', placesControllers.getPlaceById);
+router.get('/user/:uid', placesControllers.getPlacesByUserId);
+router.post('/', placesControllers.createPlace);
+router.patch('/:pid', placesControllers.updatePlace);
+router.delete('/:pid', placesControllers.deletePlace);
 
 module.exports = router;
